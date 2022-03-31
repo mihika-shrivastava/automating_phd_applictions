e8c069d5d2bd8aa9ec655c29d3e3a2d05efca319
roathena <roathena7@gmail.com>
2022-03-31 15:22:38 +0530

    few more changes

--- app.py
+++ app.py
@@ -48,86 +48,87 @@ def signin():
             user_input = {'name': username, 'email': email, 'password': hashed}
             reg.insert_one(user_input)
 
-            user_data = reg.find_one({'email': email})
-            new_email = user_data['email']
-            session["email"] = new_email
-            session["username"] = user_input['name']
-            session["logged_in"] = True
+            user_data = reg.find_one({'email': email}
+            new_email=user_data['email']
+            session["email"]=new_email
+            session["username"]=user_input['name']
+            session["logged_in"]=True
             return render_template('basic_details.html', email_val=new_email)
     return render_template('signin.html')
 
 
-@app.route("/login", methods=["POST", "GET"])
+@ app.route("/login", methods=["POST", "GET"])
 def login():
-    message = 'Please login to your account'
+    message='Please login to your account'
     if "email" in session:
-        session["logged_in"] = True
+        session["logged_in"]=True
         return render_template('home.html')
 
     if request.method == "POST":
-        username = request.form.get("name")
-        email = request.form.get("email")
-        password = request.form.get("password")
-        email_found = reg.find_one({"email": email})
+        username=request.form.get("name")
+        email=request.form.get("email")
+        password=request.form.get("password")
+        email_found=reg.find_one({"email": email})
         if email_found:
-            email_val = email_found['email']
-            passwordcheck = email_found['password']
+            email_val=email_found['email']
+            passwordcheck=email_found['password']
 
             if bc.checkpw(password.encode('utf-8'), passwordcheck):
-                session["email"] = email_val
-                session["username"] = email_found['name']
-                session["logged_in"] = True
+                session["email"]=email_val
+                session["username"]=email_found['name']
+                session["logged_in"]=True
                 return redirect(url_for('home'))
             else:
                 if "email" in session:
                     return redirect(url_for("home"))
-                message = 'Wrong password'
+                message='Wrong password'
                 return render_template('login.html', message=message)
         else:
-            message = 'Email not found'
+            message='Email not found'
             return render_template('login.html', message=message)
 
     return render_template('login.html', message=message)
 
 
-@app.route("/details", methods=["POST", "GET"])
+
+@ app.route("/details", methods=["POST", "GET"])
 def logged_in():
     if request.method == 'POST':
-        session["logged_in"] = True
-        fname = request.form.get("firstname")
-        lname = request.form.get("lastname")
-        email = request.form.get("email")
-        phno = request.form.get("contactnumber")
-        gender = request.form.get('gender')
-        birthdate = request.form.get('birthdate')
-        citizenship = request.form.get('citizenship')
-        batch = request.form.get('batch')
-        inst = request.form.get('institution')
-        year = request.form.get('year')
+        session["logged_in"]=True
+        fname=request.form.get("firstname")
+        lname=request.form.get("lastname")
+        email=request.form.get("email")
+        phno=request.form.get("contactnumber")
+        gender=request.form.get('gender')
+        birthdate=request.form.get('birthdate')
+        citizenship=request.form.get('citizenship')
+        batch=request.form.get('batch')
+        inst=request.form.get('institution')
+        year=request.form.get('year')
 
-        user_details = {'name': session['username'], 'fname': fname, 'lname': lname, 'email': email, 'phno': phno,
+        user_details={'name': session['username'], 'fname': fname, 'lname': lname, 'email': email, 'phno': phno,
                         'gender': gender, 'birthdate': birthdate, 'citizenship': citizenship, 'batch': batch, 'inst': inst, 'year': year}
 
         det.insert_one(user_details)
 
-        session['details'] = True
+        session['details']=True
 
         return render_template('home.html')
     return render_template('basic_details.html', email_val=session['email'])
 
 
-@app.route("/logout", methods=["POST", "GET"])
+@ app.route("/logout", methods=["POST", "GET"])
 def logout():
     if "email" in session:
-        session["logged_in"] = False
+        session["logged_in"]=False
         session.pop("email", None)
         return render_template("login.html")
     else:
-        session["logged_in"] = False
+        session["logged_in"]=False
         return render_template('signin.html')
 
 
-@app.route("/dashboard")
+@ app.route("/dashboard")
 def home():
     if session["logged_in"]:
         return render_template('home.html')
@@ -135,21 +136,22 @@ def home():
         return redirect('login')
 
 
-@app.route("/profile")
+
+@ app.route("/profile")
 def user_profile():
     if session["logged_in"]:
-        finder = det.find_one({'name': session['username']})
+        finder=det.find_one({'name': session['username']})
         if session['details']:
-            fname = finder['fname']
-            lname = finder['lname']
-            email = finder['email']
-            phno = finder['phno']
-            birthdate = finder['birthdate']
-            gender = finder['gender']
-            citi = finder['citizenship']
-            batch = finder['batch']
-            inst = finder['inst']
-            year = finder['year']
+            fname=finder['fname']
+            lname=finder['lname']
+            email=finder['email']
+            phno=finder['phno']
+            birthdate=finder['birthdate']
+            gender=finder['gender']
+            citi=finder['citizenship']
+            batch=finder['batch']
+            inst=finder['inst']
+            year=finder['year']
             return render_template('profile.html', fname=fname, lname=lname, email=email, phno=phno, birthdate=birthdate, gender=gender, citi=citi, batch=batch, inst=inst, year=year)
 
         return render_template('profile.html')
@@ -157,7 +159,7 @@ def user_profile():
         return redirect('login')
 
 
-@app.route("/user/documents")
+@ app.route("/user_documents")
 def doc_vault():
     return render_template('documents.html')
 
